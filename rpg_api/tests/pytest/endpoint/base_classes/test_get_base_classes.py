aec20e89f4d91bf97b053963daa3aa0e7db1b54b
Amiiiiiiiin <90452342+Amiiiiiiiin@users.noreply.github.com>
2023-12-24 12:50:35 +0100

    Tests for characters, places & classes

---
+++ rpg_api/tests/pytest/endpoint/base_classes/test_get_base_classes.py
@@ -0,0 +1,108 @@
+import pytest
+from httpx import AsyncClient
+from fastapi import status
+from rpg_api.db.postgres.factory import factories
+from typing import Any
+from rpg_api.web.api.postgres.auth import auth_utils as utils
+from rpg_api.utils import dtos
+
+url = "/api/postgres/base-classes"
+
+def get_user_header(token: str) -> dict[str, Any]:
+    """Return access token for given data."""
+    return {"Authorization": f"Bearer {token}"}
+
+@pytest.mark.anyio
+async def test_get_base_classes_empty_list(client: AsyncClient) -> None:
+    """Test get all base classes with 0 base classes in the database: 200."""
+
+    response = await client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["data"] == []
+    assert len(response.json()["data"]) == 0
+
+
+@pytest.mark.anyio
+async def test_get_base_classes_populated_list(client: AsyncClient) -> None:
+    """Test get all base classes with 1 base class in the database: 200."""
+
+    baseClass = await factories.BaseClassFactory.create()
+
+    response = await client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["data"][0]["id"] == str(baseClass.id)
+    assert response.json()["data"][0]["name"] == baseClass.name
+
+
+@pytest.mark.anyio
+async def test_get_base_classes_populated_list_2(client: AsyncClient) -> None:
+    """Test get all base classes with 2 base classes in the database: 200."""
+
+    baseClass1 = await factories.BaseClassFactory.create()
+    baseClass2 = await factories.BaseClassFactory.create()
+
+    response = await client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["data"][0]["id"] == str(baseClass1.id)
+    assert response.json()["data"][0]["name"] == baseClass1.name
+    assert response.json()["data"][1]["id"] == str(baseClass2.id)
+    assert response.json()["data"][1]["name"] == baseClass2.name
+    assert len(response.json()["data"]) == 2
+
+
+# Writing tests to ensure that only the GET method is allowed for the base-classes endpoint
+
+@pytest.mark.anyio
+async def test_base_classes_method_not_allowed_post(client: AsyncClient) -> None:
+    """Test that POST method is not allowed for the base-classes endpoint."""
+
+    response = await client.post(url, json={"name": "NewClass"})
+
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+
+
+@pytest.mark.anyio
+async def test_base_classes_method_not_allowed_put(client: AsyncClient) -> None:
+    """Test that PUT method is not allowed for the base-classes endpoint."""
+
+    response = await client.put(url, json={"name": "UpdatedClass"})
+
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+
+@pytest.mark.anyio
+async def test_base_classes_method_not_allowed_delete(client: AsyncClient) -> None:
+    """Test that DELETE method is not allowed for the base-classes endpoint."""
+
+    response = await client.delete(url)
+
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+
+
+@pytest.mark.anyio
+async def test_base_classes_method_not_allowed_patch(client: AsyncClient) -> None:
+    """Test that PATCH method is not allowed for the base-classes endpoint."""
+
+    response = await client.patch(url, json={"name": "PatchedClass"})
+
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+
+
+@pytest.mark.anyio
+async def test_base_classes_method_not_allowed_options(client: AsyncClient) -> None:
+    """Test that OPTIONS method is not allowed for the base-classes endpoint."""
+
+    response = await client.options(url)
+
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+
+
+@pytest.mark.anyio
+async def test_base_classes_method_not_allowed_head(client: AsyncClient) -> None:
+    """Test that HEAD method is not allowed for the base-classes endpoint."""
+
+    response = await client.head(url)
+
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
---
+++ rpg_api/tests/pytest/endpoint/characters/test_post_characters.py
@@ -0,0 +1,105 @@
+import pytest
+from httpx import AsyncClient
+from fastapi import status
+from rpg_api.db.postgres.factory import factories
+from typing import Any
+from rpg_api.web.api.postgres.auth import auth_utils as utils
+from rpg_api.utils import dtos
+import uuid
+
+url = "/api/postgres/characters"
+
+def get_user_header(token: str) -> dict[str, Any]:
+    """Return access token for given data."""
+    return {"Authorization": f"Bearer {token}"}
+
+
+@pytest.mark.anyio
+async def test_create_character_success(client: AsyncClient) -> None:
+    """Test successful character creation: 201."""
+
+    user = await factories.BaseUserFactory.create()
+    token = utils.create_access_token(data=dtos.TokenData(user_id=str(user.id)))
+    header = get_user_header(token)
+
+    baseClass = await factories.BaseClassFactory.create()
+    charData = {
+        "base_class_id": str(baseClass.id),
+        "gender": "male",
+        "character_name": "Garrosh Hellscream"
+    }
+
+    response = await client.post(url, headers=header, json=charData)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.anyio
+async def test_create_character_missing_data(client: AsyncClient) -> None:
+    """Test character creation with missing data: 422."""
+
+    user = await factories.BaseUserFactory.create()
+    token = utils.create_access_token(data=dtos.TokenData(user_id=str(user.id)))
+    header = get_user_header(token)
+
+    character_data = {
+        "base_class_id": str(uuid.uuid4()),
+    }
+
+    response = await client.post(url, headers=header, json=character_data)
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.anyio
+async def test_create_character_invalid_data_type(client: AsyncClient) -> None:
+    """Test character creation with invalid data types: 422."""
+
+    user = await factories.BaseUserFactory.create()
+    token = utils.create_access_token(data=dtos.TokenData(user_id=str(user.id)))
+    header = get_user_header(token)
+
+    baseClass = await factories.BaseClassFactory.create()
+    charData = {
+        "base_class_id": str(baseClass.id),
+        "gender": int(1),
+        "character_name": "Arthas Menethil"
+    }
+
+    response = await client.post(url, headers=header, json=charData)
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.anyio
+async def test_create_character_invalid_token(client: AsyncClient) -> None:
+    """Test character creation with invalid token: 401."""
+
+    header = get_user_header("invalid_token")
+
+    charData = {
+        "base_class_id": "invalid_uuid",
+        "gender": "female",
+        "character_name": "Sylvanas Windrunner"
+    }
+
+
+    response = await client.post(url, headers=header, json=charData)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.anyio
+async def test_create_character_no_token(client: AsyncClient) -> None:
+    """Test character creation with no token: 401."""
+
+    baseClass = await factories.BaseClassFactory.create()
+    charData = {
+        "base_class_id": str(baseClass.id),
+        "gender": "male",
+        "character_name": "Vol'jin"
+    }
+
+    response = await client.post(url, json=charData)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
