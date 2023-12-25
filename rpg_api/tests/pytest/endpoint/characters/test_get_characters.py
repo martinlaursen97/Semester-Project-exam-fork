0f5d5825f4e89759adcc3e1d0aff0b5978ceadec
Amiiiiiiiin <90452342+Amiiiiiiiin@users.noreply.github.com>
2023-12-25 16:26:53 +0100

    Resolved comments

--- rpg_api/tests/pytest/endpoint/characters/test_get_characters.py
+++ rpg_api/tests/pytest/endpoint/characters/test_get_characters.py
@@ -2,16 +2,9 @@ import pytest
 from httpx import AsyncClient
 from fastapi import status
 from rpg_api.db.postgres.factory import factories
-from typing import Any
-from rpg_api.web.api.postgres.auth import auth_utils as utils
-from rpg_api.utils import dtos
+from rpg_api.tests.pytest import test_utils
 
 url = "/api/postgres/characters"
-
-
-def get_user_header(token: str) -> dict[str, Any]:
-    """Return access token for given data."""
-    return {"Authorization": f"Bearer {token}"}
 
 
 @pytest.mark.anyio
@@ -19,14 +12,14 @@ async def test_get_characters_empty_list(client: AsyncClient) -> None:
     "Test get all characters with 0 characters in the database: 200."
 
     user = await factories.BaseUserFactory.create()
-    token = utils.create_access_token(data=dtos.TokenData(user_id=str(user.id)))
-    header = get_user_header(token)
+    header = test_utils.get_user_header(user.id)
 
     response = await client.get(url, headers=header)
-
     assert response.status_code == status.HTTP_200_OK
-    assert response.json()["data"] == []
-    assert len(response.json()["data"]) == 0
+
+    response_data = test_utils.get_data(response)
+    assert response_data == []
+    assert len(response_data) == 0
 
 
 @pytest.mark.anyio
@@ -34,8 +27,7 @@ async def test_get_characters_populated_list(client: AsyncClient) -> None:
     "Test get all characters with 1 character in the database: 200."
 
     user = await factories.BaseUserFactory.create()
-    token = utils.create_access_token(data=dtos.TokenData(user_id=str(user.id)))
-    header = get_user_header(token)
+    header = test_utils.get_user_header(user.id)
 
     character = await factories.CharacterFactory.create(user=user)
     # This ensures mypy knows that character_location is not None
@@ -43,28 +35,29 @@ async def test_get_characters_populated_list(client: AsyncClient) -> None:
     assert character.base_class is not None
 
     response = await client.get(url, headers=header)
-
     assert response.status_code == status.HTTP_200_OK
-    assert response.json()["data"][0]["id"] == str(character.id)
-    assert response.json()["data"][0]["gender"] == character.gender
-    assert response.json()["data"][0]["character_name"] == character.character_name
-    assert response.json()["data"][0]["alive"] == character.alive
-    assert response.json()["data"][0]["level"] == character.level
-    assert response.json()["data"][0]["xp"] == character.xp
-    assert response.json()["data"][0]["money"] == character.money
+
+    response_data = test_utils.get_data(response)
+    assert response_data[0]["id"] == str(character.id)
+    assert response_data[0]["gender"] == character.gender
+    assert response_data[0]["character_name"] == character.character_name
+    assert response_data[0]["alive"] == character.alive
+    assert response_data[0]["level"] == character.level
+    assert response_data[0]["xp"] == character.xp
+    assert response_data[0]["money"] == character.money
     assert (
-        response.json()["data"][0]["base_class"].get("name")
+        response_data[0]["base_class"].get("name")
         == character.base_class.name
     )
     assert (
-        response.json()["data"][0]["character_location"]["x"]
+        response_data[0]["character_location"]["x"]
         == character.character_location.x
     )
     assert (
-        response.json()["data"][0]["character_location"]["y"]
+        response_data[0]["character_location"]["y"]
         == character.character_location.y
     )
-    assert len(response.json()["data"]) == 1
+    assert len(response_data) == 1
 
 
 @pytest.mark.anyio
@@ -72,8 +65,7 @@ async def test_get_characters_populated_list_2(client: AsyncClient) -> None:
     "Test get all characters with 2 characters in the database: 200."
 
     user = await factories.BaseUserFactory.create()
-    token = utils.create_access_token(data=dtos.TokenData(user_id=str(user.id)))
-    header = get_user_header(token)
+    header = test_utils.get_user_header(user.id)
 
     character1 = await factories.CharacterFactory.create(user=user)
     # This ensures mypy knows that character_location is not None
@@ -85,57 +77,57 @@ async def test_get_characters_populated_list_2(client: AsyncClient) -> None:
     assert character2.base_class is not None
 
     response = await client.get(url, headers=header)
-
     assert response.status_code == status.HTTP_200_OK
-    assert response.json()["data"][0]["id"] == str(character1.id)
-    assert response.json()["data"][0]["gender"] == character1.gender
-    assert response.json()["data"][0]["character_name"] == character1.character_name
-    assert response.json()["data"][0]["alive"] == character1.alive
-    assert response.json()["data"][0]["level"] == character1.level
-    assert response.json()["data"][0]["xp"] == character1.xp
-    assert response.json()["data"][0]["money"] == character1.money
+
+    response_data = test_utils.get_data(response)
+    assert response_data[0]["id"] == str(character1.id)
+    assert response_data[0]["gender"] == character1.gender
+    assert response_data[0]["character_name"] == character1.character_name
+    assert response_data[0]["alive"] == character1.alive
+    assert response_data[0]["level"] == character1.level
+    assert response_data[0]["xp"] == character1.xp
+    assert response_data[0]["money"] == character1.money
     assert (
-        response.json()["data"][0]["base_class"].get("name")
+        response_data[0]["base_class"].get("name")
         == character1.base_class.name
     )
     assert (
-        response.json()["data"][0]["character_location"]["x"]
+        response_data[0]["character_location"]["x"]
         == character1.character_location.x
     )
     assert (
-        response.json()["data"][0]["character_location"]["y"]
+        response_data[0]["character_location"]["y"]
         == character1.character_location.y
     )
-    assert response.json()["data"][1]["id"] == str(character2.id)
-    assert response.json()["data"][1]["gender"] == character2.gender
-    assert response.json()["data"][1]["character_name"] == character2.character_name
-    assert response.json()["data"][1]["alive"] == character2.alive
-    assert response.json()["data"][1]["level"] == character2.level
-    assert response.json()["data"][1]["xp"] == character2.xp
-    assert response.json()["data"][1]["money"] == character2.money
+    assert response_data[1]["id"] == str(character2.id)
+    assert response_data[1]["gender"] == character2.gender
+    assert response_data[1]["character_name"] == character2.character_name
+    assert response_data[1]["alive"] == character2.alive
+    assert response_data[1]["level"] == character2.level
+    assert response_data[1]["xp"] == character2.xp
+    assert response_data[1]["money"] == character2.money
     assert (
-        response.json()["data"][1]["base_class"].get("name")
+        response_data[1]["base_class"].get("name")
         == character2.base_class.name
     )
     assert (
-        response.json()["data"][1]["character_location"]["x"]
+        response_data[1]["character_location"]["x"]
         == character2.character_location.x
     )
     assert (
-        response.json()["data"][1]["character_location"]["y"]
+        response_data[1]["character_location"]["y"]
         == character2.character_location.y
     )
-    assert len(response.json()["data"]) == 2
+    assert len(response_data) == 2
 
 
 @pytest.mark.anyio
 async def test_get_characters_invalid_token(client: AsyncClient) -> None:
     "Test get all characters with invalid token: 401."
 
-    header = get_user_header("invalid_token")
+    header = {"Authorization": f"Bearer invalid"}
 
     response = await client.get(url, headers=header)
-
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
@@ -144,7 +136,6 @@ async def test_get_characters_no_token(client: AsyncClient) -> None:
     "Test get all characters with no token: 401."
 
     response = await client.get(url)
-
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
@@ -153,7 +144,6 @@ async def test_characters_method_not_allowed_put(client: AsyncClient) -> None:
     """Test that PUT method is not allowed for the base classes endpoint."""
 
     response = await client.put(url, json={"name": "Character"})
-
     assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
 
 
@@ -162,7 +152,6 @@ async def test_characters_method_not_allowed_delete(client: AsyncClient) -> None
     """Test that DELETE method is not allowed for the base classes endpoint."""
 
     response = await client.delete(url)
-
     assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
 
 
@@ -171,7 +160,6 @@ async def test_characters_method_not_allowed_options(client: AsyncClient) -> Non
     """Test that OPTIONS method is not allowed for the base classes endpoint."""
 
     response = await client.options(url)
-
     assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
 
 
@@ -180,7 +168,6 @@ async def test_characters_method_not_allowed_patch(client: AsyncClient) -> None:
     """Test that PATCH method is not allowed for the base classes endpoint."""
 
     response = await client.patch(url, json={"name": "PatchedClass"})
-
     assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
 
 
@@ -189,5 +176,4 @@ async def test_characters_method_not_allowed_head(client: AsyncClient) -> None:
     """Test that HEAD method is not allowed for the base classes endpoint."""
 
     response = await client.head(url)
-
     assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
