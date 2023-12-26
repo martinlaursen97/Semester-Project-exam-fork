1669c6a246e001d7ecb8f109108ed0ba17af348a
Amiiiiiiiin <90452342+Amiiiiiiiin@users.noreply.github.com>
2023-12-27 00:55:46 +0100

    Updated tests based on feedback

--- rpg_api/tests/pytest/endpoint/characters/test_get_characters.py
+++ rpg_api/tests/pytest/endpoint/characters/test_get_characters.py
@@ -23,14 +23,13 @@ async def test_get_characters_empty_list(client: AsyncClient) -> None:
 
 
 @pytest.mark.anyio
-async def test_get_characters_populated_list(client: AsyncClient) -> None:
+async def test_get_characters_one_character(client: AsyncClient) -> None:
     """Test get all characters with 1 character in the database: 200."""
 
     user = await factories.BaseUserFactory.create()
     header = test_utils.get_user_header(user.id)
 
     character = await factories.CharacterFactory.create(user=user)
-    # This ensures mypy knows that character_location is not None
     assert character.character_location is not None
     assert character.base_class is not None
 
@@ -52,53 +51,47 @@ async def test_get_characters_populated_list(client: AsyncClient) -> None:
 
 
 @pytest.mark.anyio
-async def test_get_characters_populated_list_2(client: AsyncClient) -> None:
-    """Test get all characters with 2 characters in the database: 200."""
+async def test_get_characters_multiple_characters(client: AsyncClient) -> None:
+    """
+    Test get all characters with 3 characters in the database,
+    but the user sending the request only has access to 2: 200.
+    """
 
     user = await factories.BaseUserFactory.create()
+    user_2 = await factories.BaseUserFactory.create()
     header = test_utils.get_user_header(user.id)
 
-    character1 = await factories.CharacterFactory.create(user=user)
-    # This ensures mypy knows that character_location is not None
-    assert character1.character_location is not None
-    assert character1.base_class is not None
-    character2 = await factories.CharacterFactory.create(user=user)
-    # This ensures mypy knows that character_location is not None
-    assert character2.character_location is not None
-    assert character2.base_class is not None
+    user_characters = [
+        await factories.CharacterFactory.create(user=user),
+        await factories.CharacterFactory.create(user=user),
+    ]
+    user_2_characters = await factories.CharacterFactory.create(user=user_2)
+    assert user_2_characters.character_location is not None
+    assert user_2_characters.base_class is not None
+    for character in user_characters:
+        assert character.character_location is not None
+        assert character.base_class is not None
+
 
     response = await client.get(url, headers=header)
     assert response.status_code == status.HTTP_200_OK
 
     response_data = test_utils.get_data(response)
-    assert response_data[0]["id"] == str(character1.id)
-    assert response_data[0]["gender"] == character1.gender
-    assert response_data[0]["character_name"] == character1.character_name
-    assert response_data[0]["alive"] == character1.alive
-    assert response_data[0]["level"] == character1.level
-    assert response_data[0]["xp"] == character1.xp
-    assert response_data[0]["money"] == character1.money
-    assert response_data[0]["base_class"].get("name") == character1.base_class.name
-    assert (
-        response_data[0]["character_location"]["x"] == character1.character_location.x
-    )
-    assert (
-        response_data[0]["character_location"]["y"] == character1.character_location.y
-    )
-    assert response_data[1]["id"] == str(character2.id)
-    assert response_data[1]["gender"] == character2.gender
-    assert response_data[1]["character_name"] == character2.character_name
-    assert response_data[1]["alive"] == character2.alive
-    assert response_data[1]["level"] == character2.level
-    assert response_data[1]["xp"] == character2.xp
-    assert response_data[1]["money"] == character2.money
-    assert response_data[1]["base_class"].get("name") == character2.base_class.name
-    assert (
-        response_data[1]["character_location"]["x"] == character2.character_location.x
-    )
-    assert (
-        response_data[1]["character_location"]["y"] == character2.character_location.y
-    )
+    for index, character in enumerate(user_characters):
+        assert response_data[index]["id"] == str(character.id)
+        assert response_data[index]["gender"] == character.gender
+        assert response_data[index]["character_name"] == character.character_name
+        assert response_data[index]["alive"] == character.alive
+        assert response_data[index]["level"] == character.level
+        assert response_data[index]["xp"] == character.xp
+        assert response_data[index]["money"] == character.money
+        assert response_data[index]["base_class"].get("name") == character.base_class.name
+        assert (
+        response_data[index]["character_location"]["x"] == character.character_location.x
+        )
+        assert (
+        response_data[index]["character_location"]["y"] == character.character_location.y
+        )
     assert len(response_data) == 2
 
 
