f9b07148304e79d96abf9ff3db1428c73e318d9d
Amiiiiiiiin <90452342+Amiiiiiiiin@users.noreply.github.com>
2023-12-26 19:31:46 +0100

    Resolved PR comments

--- rpg_api/tests/pytest/endpoint/characters/test_post_characters.py
+++ rpg_api/tests/pytest/endpoint/characters/test_post_characters.py
@@ -1,18 +1,11 @@
+from typing import Any
 import pytest
 from httpx import AsyncClient
 from fastapi import status
 from rpg_api.db.postgres.factory import factories
-from typing import Any
-from rpg_api.web.api.postgres.auth import auth_utils as utils
-from rpg_api.utils import dtos
-import uuid
+from rpg_api.tests.pytest import test_utils
 
 url = "/api/postgres/characters"
-
-
-def get_user_header(token: str) -> dict[str, Any]:
-    """Return access token for given data."""
-    return {"Authorization": f"Bearer {token}"}
 
 
 @pytest.mark.anyio
@@ -20,18 +13,16 @@ async def test_create_character_success(client: AsyncClient) -> None:
     """Test successful character creation: 201."""
 
     user = await factories.BaseUserFactory.create()
-    token = utils.create_access_token(data=dtos.TokenData(user_id=str(user.id)))
-    header = get_user_header(token)
+    header = test_utils.get_user_header(user.id)
 
-    baseClass = await factories.BaseClassFactory.create()
-    charData = {
-        "base_class_id": str(baseClass.id),
+    base_class = await factories.BaseClassFactory.create()
+    character_data = {
+        "base_class_id": str(base_class.id),
         "gender": "male",
         "character_name": "Garrosh Hellscream",
     }
 
-    response = await client.post(url, headers=header, json=charData)
-
+    response = await client.post(url, headers=header, json=character_data)
     assert response.status_code == status.HTTP_201_CREATED
 
 
@@ -40,35 +31,58 @@ async def test_create_character_missing_data(client: AsyncClient) -> None:
     """Test character creation with missing data: 422."""
 
     user = await factories.BaseUserFactory.create()
-    token = utils.create_access_token(data=dtos.TokenData(user_id=str(user.id)))
-    header = get_user_header(token)
+    header = test_utils.get_user_header(user.id)
 
-    character_data = {
-        "base_class_id": str(uuid.uuid4()),
-    }
+    character_data = {}
 
     response = await client.post(url, headers=header, json=character_data)
-
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
 @pytest.mark.anyio
-async def test_create_character_invalid_data_type(client: AsyncClient) -> None:
+async def test_create_character_some_missing_data(client: AsyncClient) -> None:
+    """Test character creation with missing data: 422."""
+
+    user = await factories.BaseUserFactory.create()
+    header = test_utils.get_user_header(user.id)
+
+    base_class = await factories.BaseClassFactory.create()
+    character_data = {
+        "base_class_id": str(base_class.id),
+        "gender": "male",
+    }
+
+    response = await client.post(url, headers=header, json=character_data)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize(
+        "invalid_value",
+        [
+            1,  # int
+            9.99,  # float
+            True,  # boolean
+            None,  # NoneType
+            [1, 2, 3],  # list
+            {"x": 1},  # dict
+            (1, 2),  # tuple
+        ],
+	)
+async def test_create_character_invalid_data_type(client: AsyncClient, invalid_value: Any) -> None:
     """Test character creation with invalid data types: 422."""
 
     user = await factories.BaseUserFactory.create()
-    token = utils.create_access_token(data=dtos.TokenData(user_id=str(user.id)))
-    header = get_user_header(token)
+    header = test_utils.get_user_header(user.id)
 
-    baseClass = await factories.BaseClassFactory.create()
-    charData = {
-        "base_class_id": str(baseClass.id),
-        "gender": 1,
+    base_class = await factories.BaseClassFactory.create()
+    character_data = {
+        "base_class_id": str(base_class.id),
+        "gender": invalid_value,
         "character_name": "Arthas Menethil",
     }
 
-    response = await client.post(url, headers=header, json=charData)
-
+    response = await client.post(url, headers=header, json=character_data)
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
@@ -76,16 +90,16 @@ async def test_create_character_invalid_data_type(client: AsyncClient) -> None:
 async def test_create_character_invalid_token(client: AsyncClient) -> None:
     """Test character creation with invalid token: 401."""
 
-    header = get_user_header("invalid_token")
+    header = {"Authorization": f"Bearer invalid"}
 
-    charData = {
-        "base_class_id": "invalid_uuid",
+    base_class = await factories.BaseClassFactory.create()
+    character_data = {
+        "base_class_id": str(base_class.id),
         "gender": "female",
         "character_name": "Sylvanas Windrunner",
     }
 
-    response = await client.post(url, headers=header, json=charData)
-
+    response = await client.post(url, headers=header, json=character_data)
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
@@ -93,13 +107,12 @@ async def test_create_character_invalid_token(client: AsyncClient) -> None:
 async def test_create_character_no_token(client: AsyncClient) -> None:
     """Test character creation with no token: 401."""
 
-    baseClass = await factories.BaseClassFactory.create()
-    charData = {
-        "base_class_id": str(baseClass.id),
+    base_class = await factories.BaseClassFactory.create()
+    character_data = {
+        "base_class_id": str(base_class.id),
         "gender": "male",
         "character_name": "Vol'jin",
     }
 
-    response = await client.post(url, json=charData)
-
+    response = await client.post(url, json=character_data)
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
