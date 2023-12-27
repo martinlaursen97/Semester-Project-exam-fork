2b4f39aed04955718353e9e9237d858ce714863b
Amiiiiiiiin <90452342+Amiiiiiiiin@users.noreply.github.com>
2023-12-28 00:16:19 +0100

    hash_password & verify_password unit tests

--- rpg_api/tests/pytest/unit/test_unit_auth_utils.py
+++ rpg_api/tests/pytest/unit/test_unit_auth_utils.py
@@ -1,24 +1,93 @@
 import pytest
-from datetime import datetime, timedelta
-from unittest.mock import patch
-from rpg_api.web.api.postgres.auth.auth_utils import hash_password, verify_password, _encode_token, create_access_token, create_reset_password_token, decode_token 
-from rpg_api.settings import settings
-from rpg_api.utils import dtos
-import jwt
+from rpg_api.web.api.postgres.auth.auth_utils import (
+    hash_password,
+    verify_password,
+)
 
-def test_hash_password():
-    """Test hash_password."""
 
+def test_hash_password() -> None:
+    """Test hash_password hashes the password."""
     password = "password"
+
     hashed_password = hash_password(password)
 
     assert hashed_password != password
     assert hashed_password is not None
 
-def test_verify_password():
-    """Test verify_password."""
 
+def test_hash_password_always_different_hash() -> None:
+    """Test that hash_password always returns a different hash."""
     password = "password"
+
+    hashed_password1 = hash_password(password)
+    hashed_password2 = hash_password(password)
+
+    assert hashed_password1 != hashed_password2
+
+
+def test_hash_password_format() -> None:
+    """Test hashing with a None input."""
+    password = "password"
+
+    hashed_password = hash_password(password)
+
+    assert hashed_password.startswith("$2b$")
+
+
+def test_hash_password_above_72_chars() -> None:
+    """
+    Test hashing with a password above 72 characters.
+    bcrypt only hashes the first 72 characters and ignores the rest.
+    As it ignores anything past 72, the verify_password function
+    will return True for both passwords.
+    """
+    password_to_be_hashed = "a" * 72
+    extended_password_to_be_hashed = password_to_be_hashed + "b"
+
+    hashed_password = hash_password(password_to_be_hashed)
+    extended_hashed_password = hash_password(extended_password_to_be_hashed)
+    password = verify_password(password_to_be_hashed, hashed_password)
+    extended_password = verify_password(password_to_be_hashed, extended_hashed_password)
+
+    assert password == extended_password
+    assert password is True
+    assert extended_password is True
+
+
+@pytest.mark.parametrize(
+    "pw",
+    [
+        "",
+        "a" * 72,
+        "!@#$%^&*()",
+        "باسورد",
+        "пассворд",
+        "πάσσωορδ",
+        "פסוורד",
+        "पासवर्ड",
+        "パスワード",
+        "密码",
+        "패스워드",
+        "պասվորդ",
+        "პასვორდი",
+    ],
+)
+def test_hash_and_verify_password_edge_cases(pw: str) -> None:
+    """Test hashing with edge case passwords and different languages."""
+    password = pw
+
+    hashed_password = hash_password(password)
+    verified_password = verify_password(password, hashed_password)
+
+    assert hashed_password is not None
+    assert hashed_password != password
+    assert verified_password is True
+
+
+def test_verify_password() -> None:
+    """Test verify_password verifies the password correctly."""
+    password = "password"
+
     hashed_password = hash_password(password)
 
     assert verify_password(password, hashed_password) is True
