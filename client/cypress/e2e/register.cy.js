fae084a82cb2bd78fa45d625aba12b2e6a9c502c
martinlaursen97 <martin_laursen21@hotmail.com>
2023-12-29 20:41:29 +0100

    fix

--- client/cypress/e2e/register.cy.js
+++ client/cypress/e2e/register.cy.js
@@ -26,25 +26,25 @@ describe("User Registration and Login Workflow", () => {
     cy.url().should("include", "/login");
   });
 
-/*   it("Attempts to register a new user with an existing email", () => {
-    cy.intercept("POST", `${baseApiUrl}/auth/register`).as("userRegistration");
-
-    cy.visit(`${baseClientUrl}/login`);
-    cy.get("a:nth-child(1)").click();
-    cy.url().should("include", "/register");
-
-    cy.get("input[type='email']").type(userEmail);
-    cy.get("input[type='password']:first").type(userPassword);
-    cy.get("input[type='password']:last").type(userPassword);
-    cy.get("button[type='submit']").click();
-
-    cy.on('window:alert', (text) => {
-      expect(text).to.equal("Error: User already exists");
-    });
-
-    cy.wait("@userRegistration");
-    cy.url().should("include", "/login");
-  }); */
+  /*   it("Attempts to register a new user with an existing email", () => {
+      cy.intercept("POST", `${baseApiUrl}/auth/register`).as("userRegistration");
+  
+      cy.visit(`${baseClientUrl}/login`);
+      cy.get("a:nth-child(1)").click();
+      cy.url().should("include", "/register");
+  
+      cy.get("input[type='email']").type(userEmail);
+      cy.get("input[type='password']:first").type(userPassword);
+      cy.get("input[type='password']:last").type(userPassword);
+      cy.get("button[type='submit']").click();
+  
+      cy.on('window:alert', (text) => {
+        expect(text).to.equal("Error: User already exists");
+      });
+  
+      cy.wait("@userRegistration");
+      cy.url().should("include", "/login");
+    }); */
 
   it("Attempts to register a new user with a password that is too short", () => {
     cy.visit(`${baseClientUrl}/login`);
@@ -56,9 +56,7 @@ describe("User Registration and Login Workflow", () => {
     cy.get("input[type='password']:last").type("1");
     cy.get("button[type='submit']").click();
 
-    cy.on('window:alert', (text) => {
-      expect(text).to.equal("Password must be between 8 and 32 characters");
-    });
+    cy.checkAlert("Password must be between 8 and 32 characters");
     cy.url().should("include", "/register");
   });
 
@@ -68,23 +66,21 @@ describe("User Registration and Login Workflow", () => {
     cy.url().should("include", "/register");
 
     cy.get("input[type='email']").type(userEmail);
-    cy.get("input[type='password']:first").type("1"*33);
-    cy.get("input[type='password']:last").type("1"*33);
+    cy.get("input[type='password']:first").type("1" * 33);
+    cy.get("input[type='password']:last").type("1" * 33);
     cy.get("button[type='submit']").click();
 
-    cy.on('window:alert', (text) => {
-      expect(text).to.equal("Password must be between 8 and 32 characters");
-    });
+    cy.checkAlert("Password must be between 8 and 32 characters");
     cy.url().should("include", "/register");
   });
 
   it("Attempts to register a new user with an invalid email input", () => {
     cy.visit(`${baseClientUrl}/register`);
-    cy.get("input[type='email']").type("test"); 
+    cy.get("input[type='email']").type("test");
     cy.get("input[type='password']:first").type(userPassword);
     cy.get("input[type='password']:last").type(userPassword);
     cy.get("button[type='submit']").click();
-  
+
     cy.get("input[type='email']").should('have.prop', 'validity').then((validity) => {
       expect(validity.valid).to.be.false;
       expect(validity.typeMismatch).to.be.true;
@@ -92,21 +88,21 @@ describe("User Registration and Login Workflow", () => {
     cy.url().should("include", "/register");
   });
 
-/*   it("Attempts to register a new user without filling out all fields", () => {
-    cy.visit(`${baseClientUrl}/login`);
-    cy.get("a:nth-child(1)").click();
-    cy.url().should("include", "/register");
+  /*   it("Attempts to register a new user without filling out all fields", () => {
+      cy.visit(`${baseClientUrl}/login`);
+      cy.get("a:nth-child(1)").click();
+      cy.url().should("include", "/register");
+  
+      cy.get("button[type='submit']").click();
+  
+      cy.on('window:alert', (text) => {
+        expect(text).to.equal("Please fill out all fields");
+      });
+  
+      cy.url().should("include", "/register");
+    }); */
 
-    cy.get("button[type='submit']").click();
-
-    cy.on('window:alert', (text) => {
-      expect(text).to.equal("Please fill out all fields");
-    });
-
-    cy.url().should("include", "/register");
-  }); */
-
-  it("Logs the registered user in and out", () => {
+  it("Logs the registered user in", () => {
     cy.intercept("POST", `${baseApiUrl}/auth/login-email`).as("userLogin");
 
     cy.visit(`${baseClientUrl}/login`);
@@ -119,9 +115,6 @@ describe("User Registration and Login Workflow", () => {
     });
 
     cy.url().should("include", "/characters");
-
-    cy.get('a:nth-child(1)').click();
-    cy.url().should("include", "/login");
   });
 
   after(() => {
